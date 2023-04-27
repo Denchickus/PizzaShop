c02410bc24aacf96335a27f51ec45031fb5321d8
Denis Tarasov <denchickuz@yandex.ru>
2023-04-28 01:53:59 +0300

    Add Product migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_225954) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_220814) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
