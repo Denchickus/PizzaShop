9b75a6d635be577440176b231ec1c33e88ad34d2
Denis Tarasov <denchickuz@yandex.ru>
2023-04-28 17:19:18 +0300

    Display products step 1

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 function something(){
 
-	let x = window.localStorage.getItem('ccc');
+	let x = window.localStorage.getItem('ccc'); // x = hh['bbb']
 
-	x = x * 1 + 1;
+	x = x * 1 + 1; // x = x + 1
 
-	window.localStorage.setItem('ccc', x);
+	window.localStorage.setItem('ccc', x); // hh['bbb'] = x
 
 	alert(x);
 }
