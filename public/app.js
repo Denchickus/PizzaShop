a12021ea111123d5455dc067be9a650558e7cf68
Denis Tarasov <denchickuz@yandex.ru>
2023-05-02 23:43:17 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,17 +1,22 @@
 function something(){
 
-	let x = window.localStorage.getItem('ccc'); // x = hh['bbb']
+	// let x = window.localStorage.getItem('ccc'); // x = hh['bbb']
 
-	x = x * 1 + 1; // x = x + 1
+	// x = x * 1 + 1; // x = x + 1
 
-	window.localStorage.setItem('ccc', x); // hh['bbb'] = x
+	// window.localStorage.setItem('ccc', x); // hh['bbb'] = x
 
-	alert(x);
+	// alert(x);
 }
+
 
 function add_to_cart(id){
 
-	let x = window.localstorage.getItem('product_' + id);
+	let key = 'product_' + id;
+
+	let x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
-	window.localStorage.setItem('product_' + id, x);
+	window.localStorage.setItem(key, x);
 }
+
+
