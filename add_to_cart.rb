d1f1ff42237f7ef67fb50fdf048238358f29b90d
Denis Tarasov <denchickuz@yandex.ru>
2023-05-02 18:27:14 +0300

    some commit

---
+++ add_to_cart.rb
@@ -0,0 +1,20 @@
+hh = {}
+
+loop do
+
+	print "Enter product id: "
+	id = gets.chomp
+
+	print "Enter amount (how much items you want to order): "
+	n = gets.chomp.to_i
+
+	x = hh[id].to_i # read hash value (0 if it's not exis)
+	x = x + n       # increase by n
+
+	hh[id] = x      # set hash value
+
+	puts hh.inspect
+	puts '========================================'
+
+end
+
--- public/app.js
+++ public/app.js
@@ -11,5 +11,7 @@ function something(){
 
 function add_to_cart(id){
 
-	alert('You added pizza with id: ' + id);
+	let x = window.localstorage.getItem('product_' + id);
+	x = x * 1 + 1;
+	window.localStorage.setItem('product_' + id, x);
 }
