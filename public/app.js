b171ef897419b2856e03e392e4bf4e83ecf716f9
Denis Tarasov <denchickuz@yandex.ru>
2023-05-03 01:58:56 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -17,6 +17,26 @@ function add_to_cart(id){
 	let x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	// вывод количества item'ов в корзине
+	// alert('Items in your cart: ' + cart_get_number_of_items() );
+}
+
+function cart_get_number_of_items(){
+	let cnt = 0;
+
+	for(let i = 0; i < window.localStorage.length; i++){
+
+		let key = window.localStorage.key(i); //получаем ключ
+		let value = window.localStorage.getItem(key); // получаем значение
+
+		if(key.indexOf('product_') == 0){
+			cnt = cnt + value * 1;
+		}
+
+
+	}
+	return cnt;
 }
 
 
